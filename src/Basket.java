2e34cdbcfd891fc08765525b79701709e108d244
SerMURZIK <118277030+SerMURZIK@users.noreply.github.com>
2023-01-19 15:24:42 +0400

    Fixed mistakes

--- src/Basket.java
+++ src/Basket.java
@@ -2,7 +2,7 @@ import java.io.*;
 
 public class Basket implements Serializable {
     private final Product[] products = new Product[3];
-    private static final File fileSerial = new File("src/Files", "basket.bin");
+    private final File fileSerial = new File("src/Files", "basket.bin");
 
     public Basket(Product[] products) {
         this.products[0] = products[0];
@@ -33,9 +33,8 @@ public class Basket implements Serializable {
     }
 
     public void saveBin(File file) {
-        try {
-            FileOutputStream fos = new FileOutputStream(file);
-            ObjectOutputStream oos = new ObjectOutputStream(fos);
+        try (FileOutputStream fos = new FileOutputStream(file);
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
             oos.writeObject(this);
             fos.close();
             oos.close();
@@ -47,9 +46,8 @@ public class Basket implements Serializable {
 
     public static Basket loadFromBinFile(File file, Product[] products) {
         Basket basket = new Basket(products);
-        try {
-            FileInputStream fis = new FileInputStream(file);
-            ObjectInputStream ois = new ObjectInputStream(fis);
+        try (FileInputStream fis = new FileInputStream(file);
+             ObjectInputStream ois = new ObjectInputStream(fis)) {
             basket = (Basket) ois.readObject();
             ois.close();
             fis.close();
