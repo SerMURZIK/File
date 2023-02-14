433a8d0a669af5703c982c50ffe80e772abaaed6
SerMURZIK <118277030+SerMURZIK@users.noreply.github.com>
2023-02-14 07:41:20 +0400

    xml fixed commit

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,46 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
 import java.util.Scanner;
 
 public class Main {
+    private boolean loadEnabled;
+    private String loadFileName;
+    private String loadFormat;
+    private boolean saveEnabled;
+    private String saveFileName;
+    private String saveFormat;
+    private boolean logEnabled;
+    private String logFileName;
 
-    public static void main(String[] args) {
+    public Main() {
+        File fileXml = new File("shop.xml");
+        load(fileXml);
         Scanner scanner = new Scanner(System.in);
         Product milk = new Product("Молко", 125, 0);
         Product bread = new Product("Хлеб", 50, 1);
         Product crisps = new Product("Чипсы", 200, 2);
         Product[] products = {milk, bread, crisps};
         File fileTxt = new File("src/Files", "basket.txt");
-        File fileJson = new File("src/Files", "basket.json");
-        File fileCSV = new File("src/Files", "basket.json");
-        File fileXml = new File("src/Files", "shop.xml");
+        File fileJsonSave = new File("src/Files", saveFileName);
+        File fileJsonLoad = new File("src/Files", loadFileName);
+        File fileCSV = new File("src/Files", saveFileName);
         Basket basket = new Basket(products);
         ClientLog clientLog = new ClientLog();
-        try {
-            basket = Basket.loadFromXmlFile(fileXml, products);
-        } catch (Exception e) {
-            System.err.println("load err");
-            System.err.println(e.getMessage());
+
+        printAll();
+
+        if (loadEnabled) {
+            try {
+                basket = Basket.loadJson(fileJsonLoad, products);
+            } catch (Exception e) {
+                System.err.println("load err");
+                System.err.println(e.getMessage());
+            }
         }
 
         if (basket == null) {
@@ -48,9 +69,13 @@ public class Main {
             if (checkCase.equalsIgnoreCase("clean")) {
                 for (Product product : products) {
                     product.setAmount(0);
-                    basket.addToCartXml(product.getIndex(), product.getAmount(), fileXml);
+                    if (saveEnabled) {
+                        basket.saveJson(fileJsonSave);
+                    }
                 }
-                basket.saveXml(fileXml);
+                if (saveEnabled) {
+                    basket.saveJson(fileJsonSave);
+                }
                 continue;
             }
             if (checkCase.equalsIgnoreCase("show basket")) {
@@ -73,9 +98,13 @@ public class Main {
                 continue;
             }
             try {
-                basket.addToCartXml(numberOfProduct, amountOfProducts, fileXml);
-                clientLog.log(numberOfProduct, amountOfProducts);
-                clientLog.exportAsCSV();
+                if (saveEnabled) {
+                    basket.addToCartJson(numberOfProduct, amountOfProducts, fileJsonSave);
+                }
+                if (logEnabled) {
+                    clientLog.log(numberOfProduct, amountOfProducts);
+                    clientLog.exportAsCSV();
+                }
             } catch (Exception e) {
                 System.err.println("save in Main err");
                 System.err.println(e.getMessage());
@@ -85,5 +114,119 @@ public class Main {
         System.out.println("Ваша корзина: ");
         System.out.println();
         basket.printCart();
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    public void printAll() {
+        System.out.println(loadEnabled);
+        System.out.println(loadFileName);
+        System.out.println(loadFormat);
+        System.out.println(saveEnabled);
+        System.out.println(saveFileName);
+        System.out.println(saveFormat);
+        System.out.println(logEnabled);
+        System.out.println(logFileName);
+    }
+
+    public void load(File file) {//TODO Понять как получить конткретный node и извлечь инфу
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(file);
+            Node node = doc.getFirstChild();
+            NodeList mainList = node.getChildNodes();
+
+            for (int i = 0; i < mainList.getLength(); i++) {
+                Node node1 = mainList.item(i);
+
+                if (node1.getNodeType() == Node.ELEMENT_NODE) {
+                } else {
+                    continue;
+                }
+                NodeList childNodeList = node1.getChildNodes();
+                for (int j = 0; j < childNodeList.getLength(); j++) {
+                    if (childNodeList.item(j).getNodeType() != Node.ELEMENT_NODE) {
+                        continue;
+                    }
+
+                    if (node1.getNodeName().equals("load")) {
+                        if (childNodeList.item(j).getNodeName().equals("enabled")) {
+                            getLoadEnabled(childNodeList.item(j));
+                            continue;
+                        }
+                        if (childNodeList.item(j).getNodeName().equals("fileName")) {
+                            getLoadFileName(childNodeList.item(j));
+                            continue;
+                        }
+                        if (childNodeList.item(j).getNodeName().equals("format")) {
+                            getLoadFormat(childNodeList.item(j));
+                            continue;
+                        }
+                    }
+
+                    if (node1.getNodeName().equals("save")) {
+                        if (childNodeList.item(j).getNodeName().equals("enabled")) {
+                            getSaveEnabled(childNodeList.item(j));
+                            continue;
+                        }
+                        if (childNodeList.item(j).getNodeName().equals("fileName")) {
+                            getSaveFileName(childNodeList.item(j));
+                            continue;
+                        }
+                        if (childNodeList.item(j).getNodeName().equals("format")) {
+                            getSaveFormat(childNodeList.item(j));
+                            continue;
+                        }
+                    }
+
+                    if (node1.getNodeName().equals("log")) {
+                        if (childNodeList.item(j).getNodeName().equals("enabled")) {
+                            getLogEnabled(childNodeList.item(j));
+                            continue;
+                        }
+                        if (childNodeList.item(j).getNodeName().equals("fileName")) {
+                            getLogFileName(childNodeList.item(j));
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("load in amin err " + e.getMessage());
+        }
+    }
+
+    public void getLoadEnabled(Node node) {
+        this.loadEnabled = node.getTextContent().equals("true");
+    }
+
+    public void getLoadFileName(Node node) {
+        this.loadFileName = node.getTextContent();
+    }
+
+    public void getLoadFormat(Node node) {
+        this.loadFormat = node.getTextContent();
+    }
+
+    public void getSaveEnabled(Node node) {
+        this.saveEnabled = node.getTextContent().equals("true");
+    }
+
+    public void getSaveFileName(Node node) {
+        this.saveFileName = node.getTextContent();
+    }
+
+    public void getSaveFormat(Node node) {
+        this.saveFormat = node.getTextContent();
+    }
+
+    public void getLogEnabled(Node node) {
+        this.logEnabled = node.getTextContent().equals("true");
+    }
+
+    public void getLogFileName(Node node) {
+        this.logFileName = node.getTextContent();
     }
 }
